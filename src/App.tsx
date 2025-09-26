e17044dd2d59639a82069848f67ad6fd1c23f027
najongjine <najongjin3@hotmail.com>
2025-09-26 10:22:29 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,10 @@ function App() {
   /* inputVal 이라는 바인딩 변수랑 set 함수 만들어 주세요.
   타입은 string */
   const [inputVal, setinputVal] = useState<string>("");
+  const [evalResult, setevalResult] = useState<string>("");
   let dummy = "hello";
   useEffect(() => {
     dummy = "bye";
-
-    let data = eval("1+2");
-    console.log(`data: ${data}`);
   }, []);
 
   return (
@@ -22,17 +20,19 @@ function App() {
           value={inputVal}
           onChange={(event) => {
             let input_value = event?.target?.value ?? "";
-            dummy = input_value;
+            // 숫자와 +, -, *, /, % 만 허용하는 정규식
+            const filtered_value = input_value.replace(/[^0-9+\-*/%]/g, "");
             setinputVal(input_value);
           }}
           onKeyDown={(event) => {
             let key = event?.key;
             if (key == "Enter") {
-              alert("앤터 누름");
+              let data = eval(inputVal);
+              setevalResult(data);
             }
           }}
         />
-        <div>{dummy}</div>
+        <div> = {evalResult}</div>
         <div>내가 타이핑 한거: {inputVal}</div>
       </div>
     </div>
