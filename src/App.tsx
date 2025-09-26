c7bd5a490043d44ce53851127b9232a9d53ccd2a
najongjine <najongjin3@hotmail.com>
2025-09-26 09:06:31 +0900

    .

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  /* inputVal 이라는 바인딩 변수랑 set 함수 만들어 주세요.
+  타입은 string */
+  const [inputVal, setinputVal] = useState<string>("");
+  useEffect(() => {}, []);
+
+  return (
+    <div>
+      <div>
+        <input
+          value={inputVal}
+          onChange={(event) => {
+            setinputVal(event?.target?.value ?? "");
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
