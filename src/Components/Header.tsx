851d5f1eebec2ce65d519bf530803d1be7faf852
najongjine <najongjin3@hotmail.com>
2025-09-29 10:17:24 +0900

    .

---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+export default function Header() {
+  return (
+    <div>
+      <div>
+        <h1>Header</h1>
+      </div>
+    </div>
+  );
+}
