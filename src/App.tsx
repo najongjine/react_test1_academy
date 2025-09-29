ae310061bfc005ca0c89442cf43711f7f6fc81a0
najongjine <najongjin3@hotmail.com>
2025-09-29 10:24:11 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Calc from "./Pages/Calc";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Calc />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
