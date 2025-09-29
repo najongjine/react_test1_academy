ef95d00a7f820ffdecab137d8cd9d1fe571e3f41
najongjine <najongjin3@hotmail.com>
2025-09-29 11:09:46 +0900

    .

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <div>
       <div>
-        <h1>Header</h1>
+        <h1 className="text-3xl font-bold underline bg-blue-500 p-4 text-white">
+          Header
+        </h1>
       </div>
     </div>
   );
