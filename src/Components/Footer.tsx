25ed69ee6d6cc512a1df25aa6e361e9c3c437a9a
najongjine <najongjin3@hotmail.com>
2025-09-29 10:18:13 +0900

    .

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <div>
+        <h1>Footer</h1>
+      </div>
+    </div>
+  );
+}
