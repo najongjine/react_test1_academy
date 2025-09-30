2fe5eecf5d7ab233db9a7d0c5b9cd8c6b3ca75ca
najongjine <najongjin3@hotmail.com>
2025-09-30 10:45:12 +0900

    .

--- src/Pages/Lotto.tsx
+++ src/Pages/Lotto.tsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
 
+/* GPT 안쓴 석기시대 로또번호 만들기 */
 export default function Lotto() {
+  const [lottoNum, setlottoNum] = useState<number[]>([]);
+
+  async function onGenterateNumber(event: React.MouseEvent) {}
+
   return (
     <div className="content-margin-padding">
       <div>lotto 화면</div>
+      <div>
+        <button onClick={onGenterateNumber}>번호생성</button>
+      </div>
     </div>
   );
 }
