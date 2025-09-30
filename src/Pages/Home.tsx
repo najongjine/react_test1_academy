e13824db6fd48b18046ede3848c288175ef93a06
najongjine <najongjin3@hotmail.com>
2025-09-30 10:10:33 +0900

    .

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export default function Home() {
   return (
     <div className="content-margin-padding">
-      <div></div>
+      <div>홈 화면</div>
     </div>
   );
 }
