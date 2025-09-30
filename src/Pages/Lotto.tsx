70f3df8820b1b865c82170c7692063584458e450
najongjine <najongjin3@hotmail.com>
2025-09-30 11:41:58 +0900

    .

--- src/Pages/Lotto.tsx
+++ src/Pages/Lotto.tsx
@@ -5,11 +5,16 @@ export default function Lotto() {
   const [lottoNum, setlottoNum] = useState<number[]>([]);
 
   async function onGenterateNumber(event: React.MouseEvent) {
-    /* 번호가 바이러스처럼 증식되는걸 고쳐 봅시다 */
+    /* lottoNum 을 빈 배열로 초기화 */
+    /* filter,  */
     setlottoNum([]);
-    for (let i = 0; i < 6; i++) {
+
+    for (; lottoNum.length < 6; ) {
       let randomNum = Math.floor(Math.random() * 9999999) % 46;
       if (randomNum <= 0) randomNum = 1;
+      let duplicate = lottoNum.find((e) => e == randomNum);
+      // continue 뜻은 아래에 있는거 실행하지 마라.
+      if (duplicate) continue;
       /* state 배열에 데이터 추가하는 react 문법 */
       setlottoNum((e) => [...e, randomNum]);
     }
