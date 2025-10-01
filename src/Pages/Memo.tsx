3a7ac8b1714db32434b21c5c6dbbbe3d0ac1acd2
najongjine <najongjin3@hotmail.com>
2025-10-01 10:14:30 +0900

    .

--- src/Pages/Memo.tsx
+++ src/Pages/Memo.tsx
@@ -5,19 +5,27 @@ export default function Memo() {
   return (
     <div className="content-margin-padding">
       <div>memo 화면</div>
+
       <div>
-        <textarea
-          className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border-2 border-black focus:ring-black focus:border-black dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white-400 dark:text-white dark:focus:ring-black dark:focus:border-black"
-          placeholder="메모를 입력하세요"
-          cols={100}
-          rows={10}
-          value={memo}
-          onChange={(event) => {
-            setmemo(event?.target?.value ?? "");
-          }}
-          onKeyDown={(event) => {}}
-          maxLength={1000}
-        />
+        <form method="post" onSubmit={() => {}}>
+          <div>
+            <textarea
+              className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border-2 border-black focus:ring-black focus:border-black dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white-400 dark:text-white dark:focus:ring-black dark:focus:border-black"
+              placeholder="메모를 입력하세요"
+              cols={100}
+              rows={10}
+              value={memo}
+              onChange={(event) => {
+                setmemo(event?.target?.value ?? "");
+              }}
+              onKeyDown={(event) => {}}
+              maxLength={1000}
+            />
+          </div>
+          <div>
+            <button>저장</button>
+          </div>
+        </form>
       </div>
     </div>
   );
