50b0cf2122254eef5c8ba3d62dc69ee8eaca1ee6
najongjine <najongjin3@hotmail.com>
2025-10-01 10:25:30 +0900

    .

--- src/Pages/Memo.tsx
+++ src/Pages/Memo.tsx
@@ -22,9 +22,10 @@ export default function Memo() {
               maxLength={1000}
             />
           </div>
-          <div>
+          <br />
+          <div className="flex">
             <button
-              className="middle none center mr-4 rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              className="middle none center mr-4 rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-auto"
               data-ripple-light="true"
               onClick={(event) => {
                 event.preventDefault();
