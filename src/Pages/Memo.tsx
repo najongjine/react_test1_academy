2611d89a1cd68d6b4236729425445842d5b64cbf
najongjine <najongjin3@hotmail.com>
2025-10-01 10:05:48 +0900

    .

--- src/Pages/Memo.tsx
+++ src/Pages/Memo.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function Memo() {
+  const [memo, setmemo] = useState<string>("");
   return (
     <div className="content-margin-padding">
       <div>memo 화면</div>
@@ -9,6 +10,10 @@ export default function Memo() {
           className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border-2 border-black focus:ring-black focus:border-black dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white-400 dark:text-white dark:focus:ring-black dark:focus:border-black"
           placeholder="메모를 입력하세요"
           cols={100}
+          rows={20}
+          value={memo}
+          onChange={(event) => {}}
+          onKeyDown={(event) => {}}
         />
       </div>
     </div>
