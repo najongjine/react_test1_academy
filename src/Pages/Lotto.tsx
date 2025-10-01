2d8e74d0bff93c5d7aa7a387136b0d4d11848cc8
najongjine <najongjin3@hotmail.com>
2025-10-01 09:22:24 +0900

    .

--- src/Pages/Lotto.tsx
+++ src/Pages/Lotto.tsx
@@ -4,18 +4,18 @@ import { useEffect, useState } from "react";
 export default function Lotto() {
   const [lottoNum, setlottoNum] = useState<number[]>([]);
 
-  async function onGenterateNumber(event: React.MouseEvent) {
+  useEffect(() => {}, []);
+
+  function onGenterateNumber(event: React.MouseEvent) {
     /* lottoNum 을 빈 배열로 초기화 */
     /* filter,  */
-    setlottoNum([]);
     let tempNums: number[] = [];
-    for (; setlottoNum.length < 6; ) {
+    for (; tempNums.length < 6; ) {
       let randomNum = Math.floor(Math.random() * 9999999) % 46;
       if (randomNum <= 0) randomNum = 1;
       let duplicate = tempNums.find((e) => e == randomNum);
       // continue 뜻은 아래에 있는거 실행하지 마라.
       if (duplicate) continue;
-      alert(`dup: ${duplicate}`);
       tempNums.push(randomNum);
     }
     /* state 배열에 데이터 추가하는 react 문법 */
@@ -27,7 +27,7 @@ export default function Lotto() {
       <div>lotto 화면</div>
       <div>
         {lottoNum.map((e) => (
-          <span>{e}&nbsp;&nbsp;&nbsp;&nbsp;</span>
+          <span key={e}>{e}&nbsp;&nbsp;&nbsp;&nbsp;</span>
         ))}
       </div>
       <div>
