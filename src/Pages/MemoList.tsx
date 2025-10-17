fadf56b0e3dddb5d0cacbe4a2386e5071cf72cb5
najongjine <najongjin3@hotmail.com>
2025-10-17 09:34:23 +0900

    .

--- src/Pages/MemoList.tsx
+++ src/Pages/MemoList.tsx
@@ -1,11 +1,22 @@
 import { useEffect, useState } from "react";
 
 export default function MemoList() {
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
   useEffect(() => {
     //
   }, []);
   async function getMemoList() {
-    //
+    const fetchOption = {
+      method: "GET",
+      headers: {
+        Authorization: "",
+      },
+      body: JSON.stringify(""),
+    };
+    let res = await fetch(
+      `${API_BASE_URL}/api/board/get_memo_list`,
+      fetchOption
+    );
   }
 
   return (
