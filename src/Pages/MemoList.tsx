762f8f070663895d05cb6893a380d4580e2d9918
najongjine <najongjin3@hotmail.com>
2025-10-17 09:19:59 +0900

    .

---
+++ src/Pages/MemoList.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function MemoList() {
+  useEffect(() => {
+    //
+  });
+  async function getMemoList() {
+    //
+  }
+
+  return (
+    <div className="content-margin-padding">
+      <div>메모 List</div>
+    </div>
+  );
+}
