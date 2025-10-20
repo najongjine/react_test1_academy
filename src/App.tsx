9aaf02e94620a5ca9134db339d6ee22350ef4a48
najongjine <najongjin3@hotmail.com>
2025-10-20 10:03:27 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Home from "./Pages/Home";
 import Lotto from "./Pages/Lotto";
 import Memo from "./Pages/Memo";
 import MemoList from "./Pages/MemoList";
+import MemoUpsert from "./Pages/MemoUpsert";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/calc" element={<Calc />} />
           <Route path="/lotto" element={<Lotto />} />
           <Route path="/memo" element={<MemoList />} />
+          <Route path="/memo_upsert" element={<MemoUpsert />} />
         </Routes>
         <Footer />
       </BrowserRouter>
