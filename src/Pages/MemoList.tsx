0f66101a7239149c28a1ca54e7525935fd902fcd
najongjine <najongjin3@hotmail.com>
2025-10-21 10:33:43 +0900

    .

--- src/Pages/MemoList.tsx
+++ src/Pages/MemoList.tsx
@@ -50,7 +50,14 @@ export default function MemoList() {
             <div className="memo-info-actions">
               <div className="memo-date">{e.createdDt}</div>
               {/* 나중에 추가할 버튼 위치 */}
-              <button className="memo-button">수정</button>
+              <button
+                className="memo-button"
+                onClick={() => {
+                  onMemoUpsert(e?.id ?? 0);
+                }}
+              >
+                수정
+              </button>
               <button className="memo-button">삭제</button>
             </div>
           </div>
